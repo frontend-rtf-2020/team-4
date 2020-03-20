c1c102eb8d1a88d9ec5290b174c1194075a754a8
dartrhevan <vovasatunkin@mail.ru>
2020-03-20 23:05:15 +0500

    front instead frontend

---
+++ front/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(props) {
+    return <header>
+        <h2>
+            Project Template
+        </h2>
+    </header>;
+}
+
+//module.export = Header;
