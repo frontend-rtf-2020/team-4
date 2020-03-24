ecfc1917ac48c82689308787cceaa37ff1394169
dartrhevan <vovasatunkin@mail.ru>
2020-03-24 18:29:16 +0500

    fixed routing

---
+++ backend/routes/indexRouter.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+var express = require('express');
+var router = express.Router();
+
+/* Only for testing */
+router.get('/*', function(req, res, next) {
+    console.log('received');
+    res.sendFile(path.resolve("./front/build/index.html"));
+    //res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
