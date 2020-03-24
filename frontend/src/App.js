3e7d858c890fd93056509b97d3326e210c5313a3
dartrhevan <vovasatunkin@mail.ru>
2020-03-24 12:36:43 +0500

    components templates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter , Route , Switch} from 'react-router-dom'
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import BoardList from "./Components/BoardListPage/BoardList";
+import BoardItem from "./Components/BoardListPage/BoardItem";
+import {StartComponent} from "./Components/StartComponent";
 
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {data: ""};
-    }
-    componentDidMount() {
-        fetch('/api/test')
-            .then(d => d.text())
-            .then(t => this.setState({data: t}));
-    }
-
     render() {
         return (
             <div className="App">
                 <Header/>
                 <div>
-                    <h1>Team 4:</h1>
-                    <ol>
-                        <li>Кальская Юлия</li>
-                        <li>Лукьянов Андрей</li>
-                        <li>Сатункин Владимир</li>
-                        <li>Ткачук Денис</li>
-                        {this.state.data}
-                    </ol>
+                    <BrowserRouter>
+                        <Switch>
+                            <Route path="/item" render={(props) => <BoardItem />}/>
+                            <Route path="/list" render={(props) => <BoardList />} />
+                            <Route path='/' component={StartComponent}/>
+                        </Switch>
+                    </BrowserRouter>
                 </div>
                 <Footer/>
             </div>
---
+++ frontend/src/Components/BoardListPage/BoardItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class BoardItem extends React.Component {
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div >
+                <h2>Board</h2>
+            </div>
+        );
+    }
+}
+
+export default BoardItem;
