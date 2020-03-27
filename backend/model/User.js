5668bef173432d28b651f1b1da4125097720d363
Nordennavic <37419223+Nordennavic@users.noreply.github.com>
2020-03-27 21:44:21 +0500

    Updated models structure

--- backend/model/User.js
+++ backend/model/User.js
@@ -1,10 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const crypto = require('crypto-js');
 
 const User  = mongoose.model('User', new Schema({
-    login: String,
-    email: String,
-    password: String
+    login : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    hash : {
+        type : String,
+        required : true
+    },
+    salt : {
+        type : String,
+        required : true
+    }
 }));
 
+User.methods.setPassword = function(password){
+      this.salt = crypto.randomBytes(100).toString('hex');
+      this.hash = crypto.pbkdf2Sync(password, this.salt, 888, 64, `sha512`).toString(`hex`);
+};
+
+User.methods.validPassword = function(password){
+        let currentHash = crypto.pbkdf2Sync(password, this.salt, 888, 64, `sha512`).toString(`hex`);
+        return this.hash === currentHash;
+};
 module.exports = User;
