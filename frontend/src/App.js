530101a085e7460e5cad0febb9d8a5692af681a2
Ulano4ka <ulano4ka13@gmail.com>
2020-03-28 03:19:27 +0500

    add register and authorization forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Footer from './Components/Footer';
 import BoardList from "./Components/BoardListPage/BoardList";
 import BoardItem from "./Components/BoardListPage/BoardItem";
 import {StartComponent} from "./Components/StartComponent";
+import Autho from "./Components/Authorization/Authorization";
+import Registration from "./Components/Registration/Registration";
 
 
 class App extends React.Component {
@@ -18,6 +20,8 @@ class App extends React.Component {
                         <Switch>
                             <Route path="/item" render={(props) => <BoardItem />}/>
                             <Route path="/list" render={(props) => <BoardList />} />
+                            <Route path="/auth" render={(props) => <Autho />} />
+                            <Route path="/reg" render={(props) => <Registration />} />
                             <Route path='/' component={StartComponent}/>
                         </Switch>
                     </BrowserRouter>
