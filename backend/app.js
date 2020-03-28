a47ce9ee9a5feebcb2833a2cfd7917c031720ac7
dartrhevan <vovasatunkin@mail.ru>
2020-03-28 20:54:12 +0500

    passport pre-setting

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require("express-session");
-var apiRouter = require('./routes/routes');
+const User = require('./model/User');
+const apiRouter = require('./routes/routes');
 const indexRouter = require('./routes/indexRouter');
 //var usersRouter = require('./routes/users');
 require('dotenv').config();
-var app = express();
+const app = express();
 
 
 mongoose.connect(process.env.DB_CONNECTION_URL);//здесь должна быть строка подключения
+
+
+passport.use('register', null);
+
+passport.use('authorize', null);
+
+passport.serializeUser((user, done) => done(null, user._id));
+
+passport.deserializeUser((id, done) => User.findById(id, (err, user) => done(err, user)));
 
 app.use(logger('dev'));
 app.use(express.json());
