fa8dba7f3856e0fbed312859caf7c6373f49e2fe
Yok34 <48248922+Yok34@users.noreply.github.com>
2020-04-07 20:36:16 +0500

    return file

---
+++ frontend/src/Components/BoardListPage/BoardItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class BoardItem extends React.Component {
+    // eslint-disable-next-line no-console
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div >
+                <h2>Board</h2>
+            </div>
+        );
+    }
+}
+
+export default BoardItem;
