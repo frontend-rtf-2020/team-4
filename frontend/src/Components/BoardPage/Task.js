e2f68022f0249da9e38c349aea2bf7d25b7cd8df
dartrhevan <vovasatunkin@mail.ru>
2020-05-18 17:18:37 +0500

    deletion of useless constructor

--- frontend/src/Components/BoardPage/Task.js
+++ frontend/src/Components/BoardPage/Task.js
@@ -1,12 +1,6 @@
 import React from "react";
 
 class Task extends React.Component {
-    constructor(props) {
-        super(props);
-
-        //new Date().toDateString()
-        //this.state = { endDate: Date.parse(this.props.task.endDate.toDateString()) };
-    }
 
     componentDidMount() {
     }
